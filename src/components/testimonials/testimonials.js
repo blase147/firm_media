07ac7c8deef5e994513fce875249dda0ef5af0df
blase147 <solarmails2@gmail.com>
2024-05-03 14:49:10 +0100

    add figma

--- src/components/testimonials/testimonials.js
+++ src/components/testimonials/testimonials.js
@@ -16,8 +16,6 @@ const Testimonials = () => (
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla
       </p>
     </div>
     <div id="testimonial_card_container">
