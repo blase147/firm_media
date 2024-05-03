39fa1c3a2803f526b0fde66fa6807bf36dca8a97
blase147 <solarmails2@gmail.com>
2024-05-03 09:30:40 +0100

    add figma

--- src/components/partners/partners.js
+++ src/components/partners/partners.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './partners.scss';
+import PartnerImag1 from '../images/png/partner1.png';
+import PartnerImag2 from '../images/png/partner2.png';
+import PartnerImag3 from '../images/png/partner3.png';
+import PartnerImag4 from '../images/png/partner4.png';
+import PartnerImag5 from '../images/png/partner5.png';
 
 const Partners = () => (
-  <div>
-    <div className="partners">
-      <h1>PARTNERS</h1>
+  <div id="partners_container">
+    <div id="partners">
+      <img src={PartnerImag1} alt="partner" />
+    </div>
+    <div id="partners">
+      <img src={PartnerImag2} alt="partner" />
+    </div>
+    <div id="partners">
+      <img src={PartnerImag3} alt="partner" />
+    </div>
+    <div id="partners">
+      <img src={PartnerImag4} alt="partner" />
+    </div>
+    <div id="partners">
+      <img src={PartnerImag5} alt="partner" />
     </div>
   </div>
 );
--- src/components/portfolio videos/portfolio_videos.js
+++ src/components/portfolio videos/portfolio_videos.js
@@ -2,10 +2,28 @@ import React from 'react';
 import './portfolio_videos.scss';
 
 const PortfolioVideos = () => (
-  <div>
-    <div className="portfolio_videos">
-      <h1>PORTFOLIO VIDEOS</h1>
+  <div id="port_vid">
+    <div id="port_vid_text">
+      <h1>Adventure Awaits, Enjoy Our Captivating Photography</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla
+      </p>
+      <button type="button">Discover More</button>
     </div>
+    <iframe
+      width="100%"
+      height="520"
+      src="https://www.youtube.com/embed/cl7QisS8XH0"
+      title="How to Film Yourself and Look Cinematic"
+      // frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      // referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen
+    />
   </div>
 );
 
