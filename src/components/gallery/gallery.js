a264c69539f58896889152d37a18e2aaec4814b8
blase147 <solarmails2@gmail.com>
2024-06-23 19:05:42 +0100

    fix linters

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,49 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './gallery.scss';
 import { Link } from 'react-router-dom';
 import { fetchInstagramPosts } from './insta_api';
 import InstagramPost from './instagramPost';
 
-
-
 const Gallery = () => {
   const [instagramPosts, setInstagramPosts] = useState([]);
- 
+
   useEffect(() => {
-  const fetchPosts = async () => {
-    const postData = await fetchInstagramPosts();
-    setInstagramPosts(postData);
-  };
-  fetchPosts();
-}, []);
+    const fetchPosts = async () => {
+      const postData = await fetchInstagramPosts();
+      setInstagramPosts(postData);
+    };
+    fetchPosts();
+  }, []);
 
   return (
-  <div id="gallery">
-    <div>
-      <div className="h5_h2_p">
-        <h5>
-          <span>Our</span>
-          Gallery
-        </h5>
-        <h2>See What We Have Done</h2>
+    <div id="gallery">
+      <div>
+        <div className="h5_h2_p">
+          <h5>
+            <span>Our</span>
+            Gallery
+          </h5>
+          <h2>See What We Have Done</h2>
+        </div>
+        <Link to="/myPortfolio">
+          <button className="button" type="button">
+            See More
+          </button>
+        </Link>
       </div>
-      <Link to="/myPortfolio">
-        <button className="button" type="button">
-          See More
-        </button>
-      </Link>
-    </div>
-    <div id="gallery_container">
-      <div id="gallery_content">
-      <div className="instagram-posts">
-          {instagramPosts.map((post) => (
-            <InstagramPost key={post.id} post={post} />
-          ))}
+      <div id="gallery_container">
+        <div id="gallery_content">
+          <div className="instagram-posts">
+            {instagramPosts.map((post) => (
+              <InstagramPost key={post.id} post={post} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default Gallery;
--- src/components/gallery/instagramPost.js
+++ src/components/gallery/instagramPost.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const InstagramPost = ({ post }) => (
-  <div className="instagram-post">
-    {post.media_type === 'IMAGE' || post.media_type === 'CAROUSEL_ALBUM' ? (
-      <img src={post.media_url} alt={post.caption} />
-    ) : post.media_type === 'VIDEO' ? (
+const InstagramPost = ({ post }) => {
+  let mediaContent = null;
+
+  if (post.media_type === 'IMAGE' || post.media_type === 'CAROUSEL_ALBUM') {
+    mediaContent = <img src={post.media_url} alt={post.caption} />;
+  } else if (post.media_type === 'VIDEO') {
+    mediaContent = (
       <video controls>
         <source src={post.media_url} type="video/mp4" />
+        <track kind="captions" srcLang="en" label="English captions" default />
         Your browser does not support the video tag.
       </video>
-    ) : null}
-    {post.caption && <p>{post.caption}</p>}
-  </div>
-);
+    );
+  }
+
+  return (
+    <div className="instagram-post">
+      {mediaContent}
+      {post.caption && <p>{post.caption}</p>}
+    </div>
+  );
+};
 
 InstagramPost.propTypes = {
   post: PropTypes.shape({
